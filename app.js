a01d1b41ba36560fe0fe4c988190fc63b010a9f1
Merlinlibin <merlinlibinmerlin@gmail.com>
2024-02-19 14:52:11 +0530

    small change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const PORT = 5000;
+const PORT = process.env.PORT;
 require("./db");
 
 app.use(express.json());
